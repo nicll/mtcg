712309faa93c2533b8d5d2f061a9851b128d3378
Nicolas Klement <nicolas.klement@gmail.com>
2020-10-26 23:32:56 +0100

    Add MtcgServerPostgresAdapter for connecting to PostgreSQL databases

---
+++ MonsterTradingCardGame/MtcgServerPostgresAdapter/PostgreSqlDatabase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Dynamic;
+using Npgsql;
+
+namespace MtcgServer.Databases.Postgres
+{
+    public class PostgreSqlDatabase : IDatabase
+    {
+        private readonly string _connectionString;
+
+        public PostgreSqlDatabase(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public Player ReadPlayer(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SavePlayer(Player player, PlayerChange changes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid SearchPlayer(string name)
+        {
+            var conn = OpenConnection();
+            var cmd = new NpgsqlCommand("SELECT id FROM users WHERE name = @name", conn);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Prepare();
+
+            var result = cmd.ExecuteScalar();
+            CloseConnection(conn);
+
+            if (result is Guid playerId)
+                return playerId;
+
+            return Guid.Empty;
+        }
+
+        private NpgsqlConnection OpenConnection()
+        {
+            var conn = new NpgsqlConnection(_connectionString);
+            conn.Open();
+            return conn;
+        }
+
+        private void CloseConnection(NpgsqlConnection connection)
+            => connection.Close();
+    }
+}
