c469065487672c838101504f6389ebcf1d3cc1ee
Nicolas Klement <nicolas.klement@gmail.com>
2020-10-26 21:27:20 +0100

    Add some basic tests for REST web server

---
+++ MonsterTradingCardGame/MtcgServerTests/RestWebServerTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using RestWebServer;
+using System;
+using System.Net;
+
+namespace MtcgServerTests
+{
+    public class RestWebServerTest
+    {
+        private WebServer _web;
+
+        [SetUp]
+        public void SetupWebServer()
+        {
+            _web = new WebServer(new IPEndPoint(IPAddress.Any, 2200));
+        }
+
+        [Test]
+        public void TestRegisterStaticRoute()
+        {
+            _web.RegisterStaticRoute("GET", "/api/echo", requestContext => new RestResponse(HttpStatusCode.OK, requestContext.Payload));
+        }
+
+        [Test]
+        public void TestRegisterResourceRoute()
+        {
+            _web.RegisterResourceRoute("GET", "/api/user/%", requestContext => new RestResponse(HttpStatusCode.OK, requestContext.Resources[0]));
+        }
+    }
+}
