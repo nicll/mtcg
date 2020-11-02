5da55bc22003aab1dd205f136bd63ec1b04ba09a
Nicolas Klement <nicolas.klement@gmail.com>
2020-11-02 22:54:42 +0100

    Add four additional tests for REST webserver

--- MonsterTradingCardGame/MtcgServerTests/RestWebServerTest.cs
+++ MonsterTradingCardGame/MtcgServerTests/RestWebServerTest.cs
@@ -26,5 +26,39 @@ namespace MtcgServerTests
         {
             _web.RegisterResourceRoute("GET", "/api/user/%", requestContext => new RestResponse(HttpStatusCode.OK, requestContext.Resources[0]));
         }
+
+        [Test]
+        public void TestRegisterStaticRouteTwice()
+        {
+            Assert.Catch<ArgumentException>(() =>
+            {
+                _web.RegisterStaticRoute("GET", "/api/echo", requestContext => new RestResponse(HttpStatusCode.OK, requestContext.Payload));
+                _web.RegisterStaticRoute("GET", "/api/echo", requestContext => new RestResponse(HttpStatusCode.OK, requestContext.Payload));
+            });
+        }
+
+        [Test]
+        public void TestRegisterResourceRouteTwice()
+        {
+            Assert.Catch<ArgumentException>(() =>
+            {
+                _web.RegisterResourceRoute("GET", "/api/user/%", requestContext => new RestResponse(HttpStatusCode.OK, requestContext.Resources[0]));
+                _web.RegisterResourceRoute("GET", "/api/user/%", requestContext => new RestResponse(HttpStatusCode.OK, requestContext.Resources[0]));
+            });
+        }
+
+        [Test]
+        public void TestRegisterStaticRouteWhileListening()
+        {
+            _web.Start();
+            Assert.Catch<InvalidOperationException>(() => _web.RegisterStaticRoute("GET", "/api/echo", requestContext => new RestResponse(HttpStatusCode.OK, requestContext.Payload)));
+        }
+
+        [Test]
+        public void TestRegisterResourceRouteWhileListening()
+        {
+            _web.Start();
+            Assert.Catch<InvalidOperationException>(() => _web.RegisterResourceRoute("GET", "/api/user/%", requestContext => new RestResponse(HttpStatusCode.OK, requestContext.Resources[0])));
+        }
     }
 }
