526bd9aa5c39043fb472630e72cda22439c74a1d
Nicolas Klement <nicolas.klement@gmail.com>
2020-11-13 21:10:31 +0100

    Add 4 tests for RequestContext

---
+++ MonsterTradingCardGame/MtcgServerTests/RequestContextTest.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using RestWebServer;
+using System;
+using System.Collections.Generic;
+
+namespace MtcgServerTests
+{
+    public class RequestContextTest
+    {
+        [Test]
+        public void TestRequestContextBasic()
+        {
+            var ctx = new RequestContext("GET", "/test", new string[0], new Dictionary<string, string>(), string.Empty);
+
+            Assert.AreEqual("GET", ctx.Verb);
+            Assert.AreEqual("/test", ctx.Path);
+            Assert.AreEqual(string.Empty, ctx.Query);
+            Assert.AreEqual(string.Empty, ctx.Fragment);
+            Assert.AreEqual(0, ctx.Resources.Length);
+            Assert.AreEqual(0, ctx.Headers.Count);
+            Assert.AreEqual(string.Empty, ctx.Payload);
+        }
+
+        [Test]
+        public void TestRequestContextWithQuery()
+        {
+            var ctx = new RequestContext("GET", "/test?key=value", new string[0], new Dictionary<string, string>(), string.Empty);
+
+            Assert.AreEqual("GET", ctx.Verb);
+            Assert.AreEqual("/test", ctx.Path);
+            Assert.AreEqual("?key=value", ctx.Query);
+            Assert.AreEqual(string.Empty, ctx.Fragment);
+            Assert.AreEqual(0, ctx.Resources.Length);
+            Assert.AreEqual(0, ctx.Headers.Count);
+            Assert.AreEqual(string.Empty, ctx.Payload);
+        }
+
+        [Test]
+        public void TestRequestContextWithFragment()
+        {
+            var ctx = new RequestContext("GET", "/test#title", new string[0], new Dictionary<string, string>(), string.Empty);
+
+            Assert.AreEqual("GET", ctx.Verb);
+            Assert.AreEqual("/test", ctx.Path);
+            Assert.AreEqual(string.Empty, ctx.Query);
+            Assert.AreEqual("#title", ctx.Fragment);
+            Assert.AreEqual(0, ctx.Resources.Length);
+            Assert.AreEqual(0, ctx.Headers.Count);
+            Assert.AreEqual(string.Empty, ctx.Payload);
+        }
+
+        [Test]
+        public void TestRequestContextWithQueryAndFragment()
+        {
+            var ctx = new RequestContext("GET", "/test?key=value#title", new string[0], new Dictionary<string, string>(), string.Empty);
+
+            Assert.AreEqual("GET", ctx.Verb);
+            Assert.AreEqual("/test", ctx.Path);
+            Assert.AreEqual("?key=value", ctx.Query);
+            Assert.AreEqual("#title", ctx.Fragment);
+            Assert.AreEqual(0, ctx.Resources.Length);
+            Assert.AreEqual(0, ctx.Headers.Count);
+            Assert.AreEqual(string.Empty, ctx.Payload);
+        }
+    }
+}
