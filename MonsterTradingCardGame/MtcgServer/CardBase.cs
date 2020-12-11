4fac5b316717125eb1f67dde2fa94cc19df92168
Nicolas Klement <nicolas.klement@gmail.com>
2020-12-12 00:51:06 +0100

    Fix damage calculation for monster-vs-monster battles

--- MonsterTradingCardGame/MtcgServer/CardBase.cs
+++ MonsterTradingCardGame/MtcgServer/CardBase.cs
@@ -8,13 +8,15 @@ namespace MtcgServer
 
         public abstract int Damage { get; init; }
 
-        public int CalculateDamage(in ICard other) => (Type, other.Type) switch
-        {
-            (ElementType.Water,  ElementType.Fire)   => _CalculateDamage(other) * 2,
-            (ElementType.Fire,   ElementType.Normal) => _CalculateDamage(other) * 2,
-            (ElementType.Normal, ElementType.Water)  => _CalculateDamage(other) * 2,
-            _ => _CalculateDamage(other)
-        };
+        public int CalculateDamage(in ICard other)
+            => this is Cards.MonsterCard && other is Cards.MonsterCard
+                ? _CalculateDamage(other) : (Type, other.Type) switch
+                {
+                    (ElementType.Water,  ElementType.Fire)   => _CalculateDamage(other) * 2,
+                    (ElementType.Fire,   ElementType.Normal) => _CalculateDamage(other) * 2,
+                    (ElementType.Normal, ElementType.Water)  => _CalculateDamage(other) * 2,
+                    _ => _CalculateDamage(other)
+                };
 
         protected virtual int _CalculateDamage(in ICard other)
             => Damage;
