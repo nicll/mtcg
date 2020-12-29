9ca84f4b1951c3f2684e75194d290f763ae31f5c
Nicolas Klement <nicolas.klement@gmail.com>
2020-12-29 19:31:52 +0100

    Prevent serialization of password hash

--- MonsterTradingCardGame/MtcgServer/Player.cs
+++ MonsterTradingCardGame/MtcgServer/Player.cs
@@ -7,5 +7,9 @@ namespace MtcgServer
     /// Represents a player, their inventory and stats.
     /// </summary>
     public record Player (Guid Id, string Name, byte[] PasswordHash, string StatusText, string EmoticonText,
-        int Coins, ICollection<ICard> Stack, ICollection<ICard> Deck, int ELO, int Wins, int Losses);
+        int Coins, ICollection<ICard> Stack, ICollection<ICard> Deck, int ELO, int Wins, int Losses)
+    {
+        // prevents serialization of password hash by Json.NET
+        public bool ShouldSerializePasswordHash => false;
+    }
 }
